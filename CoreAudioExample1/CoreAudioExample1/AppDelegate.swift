77e2783a47053ff6dbc589c89fc77ba7f9c3ab2b
Thilo Mezger <thilo@macpro.local>
2017-10-29 15:48:22 +0100

    created project

---
+++ CoreAudioExample1/CoreAudioExample1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CoreAudioExample1
+//
+//  Created by Thilo Mezger on 29.10.17.
+//  Copyright © 2017 tmez030. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CoreAudioExample1/CoreAudioExample1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CoreAudioExample1
+//
+//  Created by Thilo Mezger on 29.10.17.
+//  Copyright © 2017 tmez030. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
